8ef8eeb87b3bda46019cd2b1320bbc52ab8b8381
Alonso Cárdenas <acardenas@bsd-peru.org>
2023-04-29 03:05:57 -0500

    Add support for FreeBSD

--- app/gameboard_api.py
+++ app/gameboard_api.py
@@ -244,7 +244,7 @@ class GameboardApi(BaseService):
         reference_ability = (await self.data_svc.locate('abilities', match=dict(ability_id='bf565e6a-0037-4aa4-852f-1afa222c76db')))[0]  #TODO: replace
         ability_id = str(uuid.uuid4())
         executors = []
-        for pl in ['windows', 'darwin', 'linux']:
+        for pl in ['windows', 'darwin', 'linux', 'freebsd']:
             reference_executor = reference_ability.find_executor('elasticsearch', pl)
             if not reference_executor:
                 continue
